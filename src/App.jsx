58e498b893e71017a2a1d7b4db81f4ad03dc13a1
yadullamalik <shaikyadullamalik123@gmail.com>
2022-03-14 10:42:43 +0530

    c2

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import "./App.css";
+import { Rentals } from "./Components/Rentals/Rentals";
+import { AddHouse } from "./Components/AddHouse/AddHouse";
+import { useState } from "react";
+
+function App() {
+  const [show, setShow] = useState("");
+  return (
+    <div className="App">
+      <button
+        className="toggleForm"
+        onClick={() => {
+          setShow(!show);
+        }}
+      >
+        {/* Show text Add House or Show Rentals based on state */}
+        {show ? "Show House" : "Show Form"}
+      </button>
+      {show ? <AddHouse /> : <Rentals /> }
+      {/* Show component based on state */}
+      <br />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/AddHouse/AddHouse.jsx
@@ -0,0 +1,127 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+export const AddHouse = () => {
+  const [data, setData] = useState({
+    name: "",
+    ownername: "",
+    address: "",
+    areacode: "",
+    rent: "",
+    bachelor: "",
+    married: "",
+    image: "",
+  });
+  useEffect(() => {
+    handleSubmit();
+  }, []);
+  const handleChange = (e) => {
+    let { name, value, checked, type } = e.target;
+    value = type === "checkbox" ? checked : value;
+    setData({
+      ...data,
+      [name]: value,
+    });
+  };
+  const handleSubmit = () => {
+    axios.post(`http://localhost:8080/houses`, data).then(
+      setData({
+        name: "",
+        ownername: "",
+        address: "",
+        areacode: "",
+        rent: "",
+        bachelor: "",
+        married: "",
+        image: "",
+      })
+    );
+  };
+  return (
+    <div className="addHouseContainer">
+      <form onSubmit={handleSubmit}>
+        <label>name</label>
+        <input
+          type="text"
+          className="name"
+          value={data.name}
+          name="name"
+          onChange={handleChange}
+          required
+        />
+        <br />
+        <label>ownerName</label>
+        <input
+          value={data.ownername}
+          type="text"
+          className="ownerName"
+          name="ownername"
+          onChange={handleChange}
+          required
+        />
+        <br />
+        <label>address</label>
+        <input
+          value={data.address}
+          type="text"
+          className="address"
+          name="address"
+          onChange={handleChange}
+          required
+        />
+        <br />
+        <label>areaCode</label>
+        <input
+          value={data.areacode}
+          type="text"
+          className="areaCode"
+          name="areacode"
+          onChange={handleChange}
+          required
+        />
+        <br />
+        <label>rent</label>
+        <input
+          value={data.rent}
+          type="text"
+          className="rent"
+          name="rent"
+          onChange={handleChange}
+          required
+        />
+        <br />
+        <label>preferredTenant</label>
+        <br />
+        <label>bachelor</label>
+        <input
+          checked={data.bachelor}
+          type="checkbox"
+          name="bachelor"
+          onChange={handleChange}
+          className="bachelor"
+        />
+        <br />
+        <label>married</label>
+        <input
+          checked={data.married}
+          type="checkbox"
+          className="married"
+          name="married"
+          onChange={handleChange}
+        />
+        <br />
+        <label>image</label>
+        <input
+          value={data.image}
+          type="text"
+          className="image"
+          name="image"
+          onChange={handleChange}
+          required
+        />
+        <br />
+        <input className="submitBtn" type="submit" />
+      </form>
+    </div>
+  );
+};
